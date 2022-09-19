be9ab1b4f8bcf5e13c0ff9d5ec98f8116f479f7c
OlegGlo <olegglotov99@gmail.com>
2022-09-19 17:38:35 -0400

    test

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Home from './Home';
+// import Projects from './Projects';
+import Contact from './Contact';
+// import About from './About';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Link } from "react-router-dom";
+// import NavBar from './NavBar';
+
+function App() {
+    return (
+        <div className="App">
+            {/* <NavBar /> */}
+            <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route exact path="/contact" element={<Contact />} />
+                {/* <Route exact path="/projects" element={<Projects />} /> */}
+                {/* <Route exact path="/contact" element={<Contact />} /> */}
+            </Routes>
+        </div>
+    );
+}
+
+export default App;
