4a49212d1d23f94118a0e78cb4704fae1aed4796
OlegGlo <olegglotov99@gmail.com>
2022-09-19 17:44:39 -0400

    terst

--- src/App.js
+++ src/App.js
@@ -2,22 +2,22 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Home from './Home';
-// import Projects from './Projects';
+import Projects from './Projects';
 import Contact from './Contact';
-// import About from './About';
+import About from './About';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Link } from "react-router-dom";
-// import NavBar from './NavBar';
+import NavBar from './';
 
 function App() {
     return (
         <div className="App">
-            {/* <NavBar /> */}
+            <NavBar />
             <Routes>
                 <Route exact path="/" element={<Home />} />
+                <Route exact path="/about" element={<About />} />
+                <Route exact path="/projects" element={<Projects />} />
                 <Route exact path="/contact" element={<Contact />} />
-                {/* <Route exact path="/projects" element={<Projects />} /> */}
-                {/* <Route exact path="/contact" element={<Contact />} /> */}
             </Routes>
         </div>
     );
