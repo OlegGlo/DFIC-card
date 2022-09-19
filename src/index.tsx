ae43b376b2846e46339e262325e3435fb2d974fe
OlegGlo <olegglotov99@gmail.com>
2022-09-19 16:40:02 -0400

    done link

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Web from './website';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/web" element={<Web />} />
+    </Routes>
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/website.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+import { Fade } from "react-awesome-reveal";
+// import LinkedInIcon from '@mui/icons-material/LinkedIn';
+// import EmailIcon from '@mui/icons-material/Email';
+
+function Web() {
+    return (
+        <div className="App">
+            <Fade delay={500}>
+
+                <h1>glotov.com</h1>
+
+            </Fade>
+        </div>
+    );
+}
+
+export default Web;
